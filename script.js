89010cce20d71f0b0486775577c60b6c10b36abc
Sametura028 <sametucar0028@gmail.com>
2024-04-03 08:33:30 +0300

    update

--- script.js
+++ script.js
@@ -11,26 +11,28 @@
 
 
 async function fetchData() {
-   const loader = document.getElementById("loader");
-   loader.style.display = "block";
+    const loader = document.getElementById("loader");
+    loader.style.display = "block";
 
     try {
 
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        if (!response.ok) {   alert("İstenilen Pokemon bulunamadı")
+        if (!response.ok) {
+            alert("İstenilen Pokemon bulunamadı")
             loader.style.display = "none";
-          
+
             throw new Error("İstenilen Pokemon bulunamadı");
-           
+
         }
         const data = await response.json();
         console.log(data)
         const pokemonSprite = data.sprites.front_default
-        const imgElement = document.getElementById("pokemonSprite") ;        imgElement.style.display = "none"; 
-
+        const imgElement = document.getElementById("pokemonSprite");
+        imgElement.style.display = "none";
         imgElement.src = pokemonSprite;
-        imgElement.style.display = "block";       loader.style.display = "none";
+        imgElement.style.display = "block";
+        loader.style.display = "none";
 
 
 
@@ -43,19 +45,19 @@ async function fetchData() {
 
         let index = 0;
         const pokeabilities = document.getElementById("abilities");
-    pokeabilities.innerHTML = "";
+        pokeabilities.innerHTML = "";
 
         while (index < data.abilities.length) {
             const abilities = data.abilities[index].ability.name;
             pokeabilities.innerHTML += abilities + "  ";
             index++;
-            
+
         }
 
 
         let index2 = 0;
         const poketypes = document.getElementById("pokeTypes");
-    poketypes.innerHTML = ""; 
+        poketypes.innerHTML = "";
 
         while (index2 < data.types.length) {
             const poketype = data.types[index2].type.name;
